46c3637978d6bbd9e9e5d8326a2fa01a95e531d9
kovacspe <pet.kovacs1@gmail.com>
2024-04-14 18:39:19 +0200

    Use txt file as API KEY

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
 API_KEY = None
-with open(os.path.join('.secrets','api_key')) as file:
+with open(os.path.join('.secrets', 'api_key.txt'), encoding='utf-8') as file:
     API_KEY = file.readline()
 STROM_ID = 2839
